565d77d6c461fa318e8eddf0840a1eace0c0fa02
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2023-08-07 01:49:55 +0100

    add colors

--- workflow/scripts/plots.py
+++ workflow/scripts/plots.py
@@ -19,6 +19,7 @@ def plot_line(labels: list[int],
               ivar: list[int],
               parameter: Union[Literal["acc"], Literal["Ns"], Literal["MM"]],
               condition: Union[Literal["snps"], Literal["identity"]],
+              title: str,
               dashed: bool = False,
               xlim: Union[Xlim, None] = None,
               stats: bool = False
@@ -31,7 +32,7 @@ def plot_line(labels: list[int],
     line_thikness = 2
     colors = ["#304D63", "#ED8975"]
 
-    plt.title(f"Mean {keys[parameter]}", pad=15, fontdict=fonttitle)
+    plt.title(f"{title} {keys[parameter]}", pad=15, fontdict=fonttitle)
     if xlim:
         plt.xlim(xlim.left, xlim.right)
     plt.plot(
@@ -62,15 +63,16 @@ def plot_line(labels: list[int],
     if condition == "snps":
         plt.xlabel("Number of SNPs", labelpad=10, fontdict=fontlabel)
     elif condition == "identity":
-        plt.xlabel("Nucleotide Identity (%)", labelpad=10, fontdict=fontlabel)
-        l,r = plt.xlim()
-        plt.xlim(r,l)
+        plt.xlabel("Identity frequency (%)", labelpad=10, fontdict=fontlabel)
 
-    plt.ylabel(f"Mean {keys[parameter]} across 30 samples",
+    plt.ylabel(f"{title} {keys[parameter]} across 30 samples",
                labelpad=10, fontdict=fontlabel)
     plt.xscale("linear")
     plt.legend()
 
+    llim, rlim = plt.xlim()
+
+    plt.xlim(rlim, llim)
 
     if stats:
         significance_dict = get_stats.get_stats(condition, parameter)
@@ -78,9 +80,9 @@ def plot_line(labels: list[int],
             plt.text(
                 x=item, y=max(snippy[idx], ivar[idx]), s=significance_dict[item]
             )
-        plt.savefig(f"{condition}_{parameter}_stats.png", dpi=300)
+        plt.savefig(f"{title}_{condition}_{parameter}_stats.png", dpi=300)
     else:
-        plt.savefig(f"{condition}_{parameter}.png", dpi=300)
+        plt.savefig(f"{title}_{condition}_{parameter}.png", dpi=300)
     plt.clf()
 
 
@@ -104,8 +106,9 @@ def scatter_plot(
             global_x.append(key)
             snippy_y.append(value)
             ivar_y.append(input_ivar[key][parameter][idx])
-    plt.scatter(x=global_x, y=snippy_y, label="snippy", marker="o")
-    plt.scatter(x=global_x, y=ivar_y, label="iVar", marker="v")
+
+    plt.scatter(x=global_x, y=snippy_y, label="snippy", marker="o", color="#304D63")
+    plt.scatter(x=global_x, y=ivar_y, label="iVar", marker="v", color="#ED8975")
     plt.xticks(rotation=90)
     plt.legend()
 
@@ -116,5 +119,8 @@ def scatter_plot(
     plt.ylabel(f"{keys[parameter]} per sample",
                labelpad=10, fontdict=fontlabel)
     plt.title(f"Number of {keys[parameter]}", fontdict=fonttitle)
+
+    llim, rlim = plt.xlim()
+    plt.xlim(rlim, llim)
     plt.savefig(f"scatter_{condition}_{parameter}.png", dpi=300)
     plt.clf()
